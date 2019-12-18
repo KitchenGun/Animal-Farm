041312db5db7c647a8f69bf51c69752d60791132
KitchenGun <kitchengun124@gmail.com>
2019-12-18 19:14:45 +0900

    캐릭터 배치를 위한 아이콘 제작
    선택지점으로 이동 관련 샘플
    1.마우스 클릭시 호출 함수 추가
    2.마우스 클릭시 이동하고 이동오브젝트의 부모를 이동 후 위치의 부모로 교체
    3.이동한 다음 클릭한 좌표들 초기화
    4.UI를 이용해서 캐릭터를 필드에 배치

---
+++ RiotSample0/Assets/Scripts/GameObjMoveTestStartSetting.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjMoveTestStartSetting : MonoBehaviour
+{//GameObjMoveTestStartSetting씬 시작세팅 
+    private PlayerInfo playerInfo;
+    void Start()
+    {
+        for(int i=0;i<5;i++)
+        {
+            playerInfo.SetCharId(i);
+            playerInfo.SetSlotChar(i, i);
+            playerInfo.SetCharHP(i, i);
+            playerInfo.SetCharArm(i, i);
+        }
+        
+    }
+
+}
