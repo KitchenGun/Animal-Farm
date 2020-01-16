566534e182d287ade6bbb3a9230face3ee650a92
KitchenGun <kitchengun124@gmail.com>
2020-01-16 19:53:11 +0900

    농장 디자인
    1.농장의 아이콘하고 캔버스 제작
    2.농장의 버튼 입력시 패널 생성
    3.패널 생성후 버튼을 누르면 대화 박스 생성

---
+++ RiotSample0/Assets/Scripts/ButtonClick.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Reflection;
+
+public class ButtonClick : MonoBehaviour
+{
+    private static GameObject houseUI;//정적 변수 SetActive때문에
+
+
+    private string buttonName;//현재 버튼이름 저장 
+
+    private void Awake()
+    {
+        houseUI = GameObject.FindGameObjectWithTag("HouseUI");
+        houseUI.SetActive(false);
+    }
+
+
+    public void UIButtonClick()
+    {//버튼 클릭시 실행함수
+        buttonName = EventSystem.current.currentSelectedGameObject.name;//현재 클릭한 오브젝트 이름
+        //string 문으로 함수 실행 
+        SendMessage(buttonName);
+    }
+    public void HouseButton()
+    {//집 버튼을 클릭할 경우 사용할 함수 
+        houseUI.SetActive(true);//패널 활성화
+    }
+
+
+
+}
---
+++ RiotSample0/Assets/Scripts/SimpleTextChanger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SimpleTextChanger : MonoBehaviour
+{
+    private string[] Temptext = { "1", "2", "3" };
+    public GameObject TempTextObj;
+    private TextMeshProUGUI temptext;
+    private int tempTextNum=0;
+
+    void Start()
+    {
+        temptext = TempTextObj.GetComponent<TextMeshProUGUI>();
+        temptext.text = Temptext[tempTextNum];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            temptext.text = Temptext[tempTextNum];
+            tempTextNum++;
+        }
+    }
+}
