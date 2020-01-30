e5780aaccbf6f2327cedec6598cccde9b18cd97d
KitchenGun <kitchengun124@gmail.com>
2020-01-30 21:19:39 +0900

    CSV입력 확인 완료
    1.농장의 아이콘하고 캔버스 제작
    2.농장의 버튼 입력시 패널 생성
    3.패널 생성후 버튼을 누르면 대화 박스 생성
    4.Git Hub 테스트
    5.테스트 확인
    6.CSV파일 생성 후에 입력
    7.CSV파일 공부 적용 완료

--- RiotSample0/Assets/Scripts/CSVReader.cs
+++ RiotSample0/Assets/Scripts/CSVReader.cs
@@ -7,55 +7,50 @@ using System.Text.RegularExpressions;
 public class CSVReader
 {
     static string SPLIT_RE = @",(?=(?:[^""]*""[^""]*"")*(?![^""]*""))";
-    static string LINE_SPLIT_RE = @"\r\n|\n\r|\n|\r";
+    static string LINE_SPLIT_RE = @"\r\n|\n\r|\n|\r";//줄바꾸기
     static char[] TRIM_CHARS = { '\"' };
 
-    public static List<Dictionary<string, object>> Read(string file)
+    public static List<Dictionary<string, object>> Read(string file)//빈칸이 있으면 에러가 남으로 null값이라도 넣어줘라
     {
 
         var list = new List<Dictionary<string, object>>();
-        TextAsset data = Resources.Load(file) as TextAsset;
+        TextAsset data = Resources.Load(file) as TextAsset;//파일 안에 있는 것을 유니티에서 읽기 위해 텍스트에셋 형식으로 바꿔준다
 
 
-        var lines = Regex.Split(data.text, LINE_SPLIT_RE);
+        var lines = Regex.Split(data.text, LINE_SPLIT_RE);//regex는  csv의 줄을 나눠주는 역할
 
-        if (lines.Length <= 1) return list;
+        if (lines.Length <= 1) return list;//목록이 없으면 바로 리턴
+        
+        var header = Regex.Split(lines[0], SPLIT_RE);//헤더역할
 
-        var header = Regex.Split(lines[0], SPLIT_RE);
-        for (var i = 1; i < lines.Length; i++)
+        for (var i = 1; i < lines.Length; i++)//열 탐색
         {
+            var values = Regex.Split(lines[i], SPLIT_RE);
+            if (values.Length == 0 || values[0] == "") continue;
 
-            if (lines[i].Replace(" "," ").Substring(0, 2) == "//")
+            var entry = new Dictionary<string, object>();
+            for (var j = 0; j < header.Length && j < values.Length; j++)//행 탐색
             {
-                Debug.Log(lines[i]);
-            }
-            else
-            {
-                var values = Regex.Split(lines[i], SPLIT_RE);
-                if (values.Length == 0 || values[0] == "") continue;
-
-                var entry = new Dictionary<string, object>();
-                for (var j = 0; j < header.Length && j < values.Length; j++)
+                string value = values[j];
+                value = value.Trim(TRIM_CHARS).Replace("\\", "");//TrimStart(TRIM_CHARS).TrimEnd(TRIM_CHARS).Replace("\\", ""); 괄호안에 문자를 찾아서 제거 앞쪽에서 지우고 뒤에서 지우고
+                value = value.Replace("</ br>", "\n");//괄호 안 앞에 string을 뒤에 것으로 대체
+                value = value.Replace("</ comma>", ",");
+                object finalvalue = value;
+                int n;
+                float f;
+                //나중에 형변환을 이용한 tryparse
+                if (int.TryParse(value, out n))
                 {
-                    string value = values[j];
-                    value = value.TrimStart(TRIM_CHARS).TrimEnd(TRIM_CHARS).Replace("\\", "");
-                    value = value.Replace("</ br>", "\n");
-                    value = value.Replace("</ comma>", ",");
-                    object finalvalue = value;
-                    int n;
-                    float f;
-                    if (int.TryParse(value, out n))
-                    {
-                        finalvalue = n;
-                    }
-                    else if (float.TryParse(value, out f))
-                    {
-                        finalvalue = f;
-                    }
-                    entry[header[j]] = finalvalue;
+                    finalvalue = n;
                 }
-                list.Add(entry);
+                else if (float.TryParse(value, out f))
+                {
+                    finalvalue = f;
+                }
+                entry[header[j]] = finalvalue;//키에 값을 넣는다
             }
+            list.Add(entry);//리스트에 추가한다
+            
         }
         return list;
     }
--- RiotSample0/Assets/Scripts/GameObjMoveTestStartSetting.cs
+++ RiotSample0/Assets/Scripts/GameObjMoveTestStartSetting.cs
@@ -5,20 +5,25 @@ using UnityEngine;
 public class GameObjMoveTestStartSetting : MonoBehaviour
 {//GameObjMoveTestStartSetting씬 시작세팅 
     private PlayerInfo playerInfo;
+
+    public int tempint;
     void Start()
     {
         List<Dictionary<string, object>> data = CSVReader.Read("TempData");
         for (var i = 0; i < data.Count; i++)
         {
-            Debug.Log("index"+i.ToString() + data[i]["name"] + " " + data[i]["ID"]);
+            tempint = (int)data[i]["ID"];
+            Debug.Log(data[i]["name"] + " " + tempint);
         }
-        for (int i=0;i<5;i++)
-        {
-            playerInfo.SetCharId(i);
-            playerInfo.SetSlotChar(i, i);
-            playerInfo.SetCharHP(i, i);
-            playerInfo.SetCharAP(i, i);
-        }
+
+
+        //for (int i=0;i<5;i++)
+        //{
+        //    playerInfo.SetCharId(i);
+        //    playerInfo.SetSlotChar(i, i);
+        //    playerInfo.SetCharHP(i, i);
+        //    playerInfo.SetCharAP(i, i);
+        //}
         
     }
 
