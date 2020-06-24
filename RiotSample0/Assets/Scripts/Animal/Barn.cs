cf61dc0d8e8d0c1548aa09da6a808d477b8181bd
KitchenGun <kitchengun124@gmail.com>
2020-06-24 23:05:24 +0900

    러닝콘서트 빌드 버전

---
+++ RiotSample0/Assets/Scripts/Animal/Barn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barn : MonoBehaviour
+{
+
+    private GameCombatManager GM;
+
+
+    private void Start()
+    {
+        GM = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameCombatManager>();
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.gameObject.tag == "Enemy")
+        {
+            GM.Lose();
+        }
+    }
+
+}
