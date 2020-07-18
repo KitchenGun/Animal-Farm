494dcaf2634681b2025d3ac6a23b14e08b16a253
KitchenGun <kitchengun124@gmail.com>
2020-07-18 11:49:04 +0900

    계란 범위 공격
    1.피격시 빨간색으로 변함
    2.계란 범위 공격

--- RiotSample0/Assets/Scripts/Animal/Egg.cs
+++ RiotSample0/Assets/Scripts/Animal/Egg.cs
@@ -2,31 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Egg : MonoBehaviour
+public class Egg : Animal
 {
     [SerializeField]
     private Animator EggAni;
+    
+    private List<GameObject> hitObj = new List<GameObject>();
 
     private void Start()
     {
+        //AnimalID=
+        AP=PlayerPrefs.GetInt(AnimalID + "AP");
+        ATKRange = PlayerPrefs.GetInt(AnimalID + "ATKRange");
         Invoke("selfDestroy", 3f);
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.transform.gameObject.name=="Plane")
+        
+        if (collision.transform.gameObject.name=="Plane")
         {
+            EggAni.SetTrigger("Hit");//애니메이션
+            selfDestroy();
+        }
+        else if(collision.transform.gameObject.CompareTag("Friendly"))
+        {
+            EggAni.SetTrigger("Hit");//애니메이션
             selfDestroy();
         }
         if (collision.transform.gameObject.tag == "Enemy")
         {
-            collision.transform.gameObject.GetComponent<Enemy>().SendMessage("Hit", 10);
-            selfDestroy();
+            this.GetComponent<SphereCollider>().radius = 1.5f;//ATKRange;
+            hitObj.Add(collision.gameObject);
+            this.GetComponent<Rigidbody>().isKinematic = true;
+            EggAni.SetTrigger("Hit");//애니메이션
+            //대미지주기
+            Invoke("SetDamage",0.3f);
+        }
+
+    }
+
+    private void SetDamage()
+    {
+        foreach (GameObject enemy in hitObj)
+        {
+            if (enemy != null)
+            {//타겟이 죽는 경우는 무시한다
+                enemy.GetComponent<Enemy>().SendMessage("Hit", AP);
+            }
         }
     }
 
     private void selfDestroy()
     {
-        EggAni.SetTrigger("Hit");
         Destroy(this.gameObject, 0.5f);
     }
 }
