d8a7e32605c08557ae1af18e034b822de96bff4a
KitchenGun <kitchengun124@gmail.com>
2020-08-03 18:38:18 +0900

    헛간 애니메이션추가
    1.쥐오브젝트 추가 계란 CSV연동
    2.헛간 애니메이션 추가

--- RiotSample0/Assets/Scripts/Animal/Barn.cs
+++ RiotSample0/Assets/Scripts/Animal/Barn.cs
@@ -6,21 +6,29 @@ public class Barn : MonoBehaviour
 {
 
     private GameCombatManager GM;
-
+    private Animator BarnAnimator;
 
     private void Start()
-    {
+    {//변수 찾기
         GM = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameCombatManager>();
-
+        BarnAnimator = this.GetComponent<Animator>();
     }
 
 
     private void OnTriggerEnter(Collider other)
-    {
+    {//충돌시 파괴함수 실행 추후에 변경가능
         if (other.transform.gameObject.tag == "Enemy")
         {
-            GM.Lose();
+            StartCoroutine(BarnDestroy());
         }
+    }
+
+    private IEnumerator BarnDestroy()
+    {//헛간 파괴
+        BarnAnimator.SetTrigger("Destroy");
+        yield return new WaitForSeconds(2f);
+        GM.Lose();
+        StopCoroutine(BarnDestroy());
     }
 
 }
